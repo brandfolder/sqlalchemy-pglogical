e368e8945060088a24c864e6fb5a8cc9e4dd9a03
Ben Berry <benjamin.berry@smartsheet.com>
2023-09-08 11:50:49 -0700

    use dollar-quoting to improve/sidestep escaping

--- sqlalchemy_pglogical/replicate_ddl.py
+++ sqlalchemy_pglogical/replicate_ddl.py
@@ -14,8 +14,9 @@ def ddl_to_replicate_command(ddl: str) -> str:
     Given a DDL command as a string, return a pglogical replicate command for that DDL
     """
     # TODO: allow specifying the replication sets
-    # TODO: better escaping for the ddl command
-    return "SELECT pglogical.replicate_ddl_command('%s')" % ddl
+    return f"""SELECT pglogical.replicate_ddl_command($sqlalchemypglogical$
+    {ddl}
+    $sqlalchemypglogical$)"""
 
 
 def make_func(ddl):
