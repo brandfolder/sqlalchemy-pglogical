8ad1b1c46852cdf59799ff81c3df95ca53cfc680
Ben Berry <benjamin.berry@smartsheet.com>
2023-10-11 09:24:28 -0700

    add tests for index creation/deletion

--- tests/integration/test_alembic_migrations.py
+++ tests/integration/test_alembic_migrations.py
@@ -10,6 +10,18 @@ from sqlalchemy.orm import declarative_base, sessionmaker
 wait_lsn = text("select pglogical.wait_slot_confirm_lsn(NULL, NULL)")
 
 
+def wait_for_query_truthiness(sessionmaker_, query, truthy=True):
+    for _ in range(30):
+        # make the session inside the loop to make sure we're not holding any cursors, etc.
+        with sessionmaker_() as session:
+            session.execute(wait_lsn)
+            maybe_results = session.execute(query)
+            result = maybe_results.first()
+            if (result is not None) == truthy:
+                break
+    return result
+
+
 @pytest.mark.flaky(max_runs=3)
 def test_create_with_alembic(clean_primary, clean_secondary):
     primary_engine = create_engine(clean_primary)
@@ -30,16 +42,7 @@ def test_create_with_alembic(clean_primary, clean_secondary):
         primary_session.execute(wait_lsn)
         primary_tables = primary_session.execute(table_query)
         primary_table = primary_tables.first()
-    for _ in range(30):
-        # make the session inside the loop to make sure we're not holding any cursors, etc.
-        with LogicalSession() as logical_session:
-            logical_session.execute(wait_lsn)
-            logical_tables = logical_session.execute(table_query)
-            logical_table = logical_tables.first()
-            if logical_table is not None:
-                break
-            time.sleep(0.5)
-
+    logical_table = wait_for_query_truthiness(LogicalSession, table_query)
     assert primary_table is not None
     assert logical_table is not None
 
@@ -67,14 +70,59 @@ def test_add_column_with_alembic(clean_primary, clean_secondary):
         primary_session.execute(wait_lsn)
         primary_columns = primary_session.execute(column_query)
         primary_column = primary_columns.first()
-    for _ in range(30):
-        with LogicalSession() as logical_session:
-            logical_session.execute(wait_lsn)
-            logical_columns = logical_session.execute(column_query)
-            logical_column = logical_columns.first()
-            if logical_column is not None:
-                break
-            time.sleep(0.5)
-
+    logical_column = wait_for_query_truthiness(LogicalSession, column_query)
     assert primary_column is not None
     assert logical_column is not None
+
+
+@pytest.mark.flaky(max_runs=3)
+def test_drop_column_with_alembic(clean_primary, clean_secondary):
+    config = Config(pathlib.Path(__file__).parent / "alembic.ini")
+    command.upgrade(config, "4b361a7c7bf3")
+    logical_engine = create_engine(clean_secondary)
+    LogicalSession = sessionmaker(logical_engine)
+
+    column_query = text(
+        """SELECT
+                    column_name
+                FROM
+                    information_schema.columns
+                WHERE
+                    table_name = 'account'
+                    and column_name = 'new_column';"""
+    )
+
+    logical_column = wait_for_query_truthiness(LogicalSession, column_query)
+    assert logical_column is not None
+
+    command.downgrade(config, "b67a71ccf11e")
+
+    logical_column = wait_for_query_truthiness(
+        LogicalSession, column_query, truthy=False
+    )
+    assert logical_column is None
+
+
+@pytest.mark.flaky(max_runs=3)
+def test_add_index(clean_primary, clean_secondary):
+    config = Config(pathlib.Path(__file__).parent / "alembic.ini")
+    command.upgrade(config, "c3d4de521f9a")
+    logical_engine = create_engine(clean_secondary)
+    LogicalSession = sessionmaker(logical_engine)
+
+    index_query = text(
+        """SELECT
+                    indexname
+                FROM
+                    pg_indexes
+                WHERE
+                    indexname = 'idx_account_name';"""
+    )
+
+    logical_index = wait_for_query_truthiness(LogicalSession, index_query)
+    assert logical_index is not None
+
+    command.downgrade(config, "4b361a7c7bf3")
+
+    logical_index = wait_for_query_truthiness(LogicalSession, index_query, truthy=False)
+    assert logical_index is None
